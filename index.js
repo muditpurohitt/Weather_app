0c8b0d4830ba9fd4f22eaa29005d610f64ae5e07
Mudit <muditp22@gmail.com>
2023-12-15 00:57:45 +0530

    More updates to index.js....version 1

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const tempField = document.querySelector(".temperature");
 const locationField = document.querySelector(".locationTime p");
 const dateTime = document.querySelector(".locationTime span");
-const emoticon = document.querySelector(".weather img");
-const weather = document.querySelector(".weather span");
+const emoticon = document.querySelector(".condition img");
+const weather = document.querySelector(".condition span");
 const searchField = document.querySelector(".search_field");
 const form = document.querySelector("form");
 
-let target = 'Delhi';
-
+let target = 'Rajsamand';
 form.addEventListener("submit", callback);
-function callback(){
-    target = searchField.ariaValueMax;
+
+function callback(e){
+    e.preventDefault();
+    target = searchField.value;
     fetchData(target);
 }
+
 async function fetchData(target){
     try{
         let url = `https://api.weatherapi.com/v1/current.json?key=72a1555b5c6e4af2926161132231412&q=${target}&aqi=no`;
@@ -24,11 +26,45 @@ async function fetchData(target){
         let time = data.location.localtime;
         let condition = data.current.condition.text;
         let emoji = data.current.condition.icon;
+        
+        updateDOM(current_temp, location, time, condition, emoji);
 
         console.log(current_temp, location, time, condition, emoji);
     }
     catch(error){
         console.log(error);
+    }
+}
+
+function updateDOM(temp, location, time, condition, emoji){
+    tempField.innerHTML = temp;
+    locationField.innerHTML = location;
+    emoticon.src = emoji;
+    weather.innerHTML = condition;
+
+    let tm = time.split(" ")[1];
+    let date = time.split(" ")[0];
+    let day = fetchDay(new Date(date).getDay());
+
+    dateTime.innerHTML = `${tm}  ${date}  ${day}`;
+}
+
+function fetchDay(num){
+    switch(num){
+        case 0:
+            return "Sunday";
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
     }
 }
 fetchData(target);
